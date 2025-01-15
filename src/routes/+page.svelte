098e2eaffa2b01fad253fc42359b0b35184294d7
impappdavid <impappdavid@gmail.com>
2025-01-15 11:08:47 +0100

    Home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<div class="w-full flex flex-col gap-6 justify-center items-center h-screen">
+    <h1 class="text-5xl font-bold font-mono">david/ref</h1>
+    <div class="flex flex-col gap-3 text-center">
+        <a href="#" class="text-2xl tracking-wide hover:tracking-widest hover:font-semibold hover:text-yellow-400 transition-all">[Javascript]</a>
+        <a href="#" class="text-2xl tracking-wide hover:tracking-widest hover:font-semibold hover:text-blue-400 transition-all">[Typescript]</a>
+        <a href="#" class="text-2xl tracking-wide hover:tracking-widest hover:font-semibold hover:text-yellow-400 transition-all">[Python]</a>
+        <a href="#" class="text-2xl tracking-wide hover:tracking-widest hover:font-semibold hover:text-orange-500 transition-all">[Sveltekit]</a>
+        <a href="#" class="text-2xl tracking-wide hover:tracking-widest hover:font-semibold hover:text-red-400 transition-all">[Nestjs]</a>
+    </div>
+</div>
